2943e7b9108b91bdfe05e2cf591b5c5cdae40d81
jmgnve <jmg@nve.no>
2017-09-23 11:11:46 +0200

    fixed calibration and data handling to VannModels

--- calibration_operational.jl
+++ calibration_operational.jl
@@ -1,6 +1,7 @@
+
 # Load packages
 
-using Vann
+using VannModels
 using PyPlot
 using DataFrames
 using ExcelReaders
@@ -8,29 +9,17 @@ using JLD
 using CSV
 using ProgressMeter
 
-# Settings
-
-opt = Dict("epot_choice" => :epot_monthly,
-           "snow_choice" => :TinBasic,
-           "hydro_choice" => :Gr4j,
-           "force_states" => true,
-           "tstep" => 24.0,
-           "warmup" => 3*365,
-           "path_save" => "/home/jmg/flood_forecasting/model_calib",
-           "path_inputs" => "/home/jmg/flood_forecasting/model_input")
-
-# Create folders for saving results
-
-mkpath(opt["path_save"] * "/tables")
-mkpath(opt["path_save"] * "/figures")
-mkpath(opt["path_save"] * "/param_snow")
-mkpath(opt["path_save"] * "/param_hydro")
-mkpath(opt["path_save"] * "/model_data")
 
 # Calibrate all stations for one experiment
 
 function calib_all_stations(opt)
 
+    # Create folders for saving results
+    
+    mkpath(joinpath(opt["path_save"], string(opt["model_choice"]), "figures"))
+    mkpath(joinpath(opt["path_save"], string(opt["model_choice"]), "tables"))
+    mkpath(joinpath(opt["path_save"], string(opt["model_choice"]), "model_data"))
+    
     # Empty dataframes for summary statistics
 
     df_calib = DataFrame(Station = String[], NSE = Float64[], KGE = Float64[])
@@ -38,31 +27,39 @@ function calib_all_stations(opt)
     # Loop over all watersheds
 
     dir_all = readdir(opt["path_inputs"])
-
-    @showprogress 1 "Calibrating models..." for dir_cur in dir_all
+    
+    @showprogress 1 "Calibrating $(opt["model_choice"])..." for dir_cur in dir_all
 
         # Load data
 
-        date, tair, prec, q_obs, frac = 0, 0, 0, 0, 0
+        date, tair, prec, q_obs, frac_lus, frac_area, elev = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
 
         try
-            date, tair, prec, q_obs, frac = load_operational("$(opt["path_inputs"])/$dir_cur")
+            
+            path = joinpath(opt["path_inputs"], dir_cur)
+
+            date, tair, prec, q_obs, frac_lus, frac_area, elev = load_data(path)
+            
         catch
+
             info("Unable to read files in directory $(dir_cur)\n")
             continue
+
         end
 
         # Compute potential evapotranspiration
+        
+        lat = 60.0   # TODO: Read latitude in NveData, and read in load_data
+        
+        epot = oudin(date, tair, lat, frac_area)
 
-        epot = eval(Expr(:call, opt["epot_choice"], date))
+        # Rough precipitation correction step   TODO: This will not be consistent with the calibrated pcorr
 
-        # Precipitation correction step
+        #= if sum(~isnan.(q_obs)) > (3*365)
 
-        if sum(~isnan(q_obs)) > (3*365)
+            ikeep = .~isnan.(q_obs)
 
-            ikeep = ~isnan(q_obs)
-
-            prec_tmp = sum(prec .* repmat(frac, 1, size(prec,2)), 1)
+            prec_tmp = sum(prec .* repmat(frac_area, 1, size(prec,2)), 1)
 
             prec_sum = sum(prec_tmp[ikeep])
             q_sum = sum(q_obs[ikeep])
@@ -77,87 +74,132 @@ function calib_all_stations(opt)
             warn("Not enough runoff data for calibration (see folder $dir_cur)")
             continue
 
-        end
+        end =#
 
-        # Initilize model
+        # Initilize input object
 
-        st_snow = eval(Expr(:call, opt["snow_choice"], opt["tstep"], date[1], frac))
-        st_hydro = eval(Expr(:call, opt["hydro_choice"], opt["tstep"], date[1]))
+        input = InputPTE(prec, tair, epot)
+
+        # Initilize model object
+
+        model = eval(Expr(:call, opt["model_choice"], opt["tstep"], date[1], frac_lus))
 
         # Run calibration
 
-        param_snow, param_hydro = run_model_calib(st_snow, st_hydro, date, tair, prec, epot, q_obs;
-                                                force_states = opt["force_states"], warmup = opt["warmup"])
+        param_tuned = run_model_calib(model, input, q_obs, warmup = opt["warmup"], verbose = :verbose)
 
-        println("Snow model parameters: $param_snow")
-        println("Hydro model parameters: $param_hydro")
+        # Rerun model with optimized parameters
 
-        # Reinitilize model
+        init_states!(model)
 
-        st_snow = eval(Expr(:call, opt["snow_choice"], opt["tstep"], date[1], param_snow, frac))
-        st_hydro = eval(Expr(:call, opt["hydro_choice"], opt["tstep"], date[1], param_hydro))
+        set_params!(model, param_tuned)
 
         # Run model with best parameter set
 
-        q_sim, st_snow, st_hydro = run_model(st_snow, st_hydro, date, tair, prec, epot; return_all = true)
+        q_sim = run_model(model, input)
 
-        # Store results in data frame
+        # Store results in dataframe
 
-        q_obs = round(q_obs, 2)
-        q_sim = round(q_sim, 2)
+        q_obs = round.(q_obs, 2)
+        q_sim = round.(q_sim, 2)
 
         df_res = DataFrame(date = Dates.format(date,"yyyy-mm-dd"), q_sim = q_sim, q_obs = q_obs)
 
-        # Save results to txt file
+        # Save dataframe to txt file
 
-        file_save = dir_cur[1:end-5]
+        stat_name = dir_cur[1:end-5]
+        
+        file_save = joinpath(opt["path_save"], string(opt["model_choice"]), "tables", "$(stat_name)_station.txt")
 
-        writetable(string(opt["path_save"], "/tables/", file_save, "_station.txt"), df_res, quotemark = '"', separator = '\t')
-
+        writetable(file_save, df_res, quotemark = '"', separator = '\t')
+        
         # Plot results
 
         ioff()
 
-        file_name = string(opt["path_save"], "/figures/", file_save, "_hydro.png")
+        file_save = joinpath(opt["path_save"], string(opt["model_choice"]), "figures", "$(stat_name)_runoff.png")
 
-        plot_sim(st_hydro; q_obs = q_obs, file_name = file_name)
+        fig = plt[:figure](figsize = (12,7))
 
-        file_name = string(opt["path_save"], "/figures/", file_save, "_snow.png")
+        plt[:style][:use]("ggplot")
+        
+        ikeep = opt["warmup"]:length(date)
 
-        plot_sim(st_snow; file_name = file_name)
+        plt[:plot](date[ikeep], q_sim[ikeep], linewidth = 1.2, color = "r", label = "Sim")
+        plt[:title]("Station: $(stat_name)")
+        plt[:ylabel]("Runoff (mm/day)")
 
-        # Compute summary statistics
+        if ~isempty(q_obs)
+            kge_res = kge(q_sim[ikeep], q_obs[ikeep])
+            nse_res = nse(q_sim[ikeep], q_obs[ikeep])
+            kge_res = round(kge_res, 2)
+            nse_res = round(nse_res, 2)
+            plt[:plot](date[ikeep], q_obs[ikeep], linewidth = 1.2, color = "b", label = "Obs")
+            plt[:legend]()
+            plt[:title]("Station: $(stat_name) | KGE = $(kge_res) | NSE = $(nse_res)")
+        else
+            kge_res = 0.0
+            nse_res = 0.0
+        end
 
-        station = dir_cur[1:end-5]
-        kge_res = kge(q_sim[opt["warmup"]:end], q_obs[opt["warmup"]:end])
-        nse_res = nse(q_sim[opt["warmup"]:end], q_obs[opt["warmup"]:end])
+        savefig(file_save)        
 
-        push!(df_calib, [station nse_res kge_res])
+        close(fig)
 
-        # Save parameter values
+        # Add to dataframe
 
-        writedlm(opt["path_save"] * "/param_snow/" * file_save * "_param_snow.txt", param_snow)
-        writedlm(opt["path_save"] * "/param_hydro/" * file_save * "_param_hydro.txt", param_hydro)
+        push!(df_calib, [stat_name nse_res kge_res])
 
-        st_snow = eval(Expr(:call, opt["snow_choice"], opt["tstep"], date[1], param_snow, frac))
-        st_hydro = eval(Expr(:call, opt["hydro_choice"], opt["tstep"], date[1], param_hydro))
 
-        jldopen(opt["path_save"] * "/model_data/" * file_save * "_modeldata.jld", "w") do file
-            addrequire(file, Vann)
-            write(file, "st_snow", st_snow)
-            write(file, "st_hydro", st_hydro)
+
+        # TODO: Save model parameters here perhaps ???
+
+
+
+        # Save model object (including parameter values)
+
+        init_states!(model)
+
+        file_save = joinpath(opt["path_save"], string(opt["model_choice"]), "model_data", "$(stat_name)_modelobj.jld")
+
+        jldopen(file_save, "w") do file
+            addrequire(file, VannModels)
+            write(file, "model", model)
         end
 
     end
 
-    writetable(string(opt["path_save"], "/summary_calib_period.txt"), df_calib, quotemark = '"', separator = '\t')
+    file_save = joinpath(opt["path_save"], string(opt["model_choice"]), "summary_calib.txt")
+
+    writetable(file_save, df_calib, quotemark = '"', separator = '\t')
 
 end
 
+
+# Settings for gr4j
+
+opt = Dict("epot_choice" => :oudin,
+           "model_choice" => :model_gr4j,
+           "tstep" => 24.0,
+           "warmup" => 3*365,
+           "path_save" => "/hdata/fou/jmg/flood_forecasting/model_calib",
+           "path_inputs" => "/hdata/fou/jmg/flood_forecasting/model_input")
+          
 # Run the calibration
 
 calib_all_stations(opt)
 
 
+# Settings for hbv_ligt
 
+opt = Dict("epot_choice" => :oudin,
+           "model_choice" => :model_hbv_light,
+           "tstep" => 24.0,
+           "warmup" => 3*365,
+           "path_save" => "/hdata/fou/jmg/flood_forecasting/model_calib",
+           "path_inputs" => "/hdata/fou/jmg/flood_forecasting/model_input")
+
+# Run the calibration
+
+calib_all_stations(opt)
 
